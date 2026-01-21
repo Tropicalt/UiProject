83986b6b4273fe516a4fa71b9810ecdb1fa90575
Manamite <bmeyer@ftstudent.org>
2026-01-21 12:21:55 -0500

    googg

---
+++ Scenes/Score.cs
@@ -0,0 +1,15 @@
+using Godot;
+using System;
+
+private AnimatedSprite2D _score;
+
+public partial class Score : AnimatedSprite2D
+{
+	
+	
+	
+	public void UpdatePoints(int points) {
+		.Frame += 1;
+	}
+	
+}
